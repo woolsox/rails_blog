4649ccdf62cb4befa9a6b491dc741381789203d5
Colin Di Meo <dimeo.colin@gmail.com>
2017-09-18 16:32:12 -0400

    added error messages for invalid form submissions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,11 @@ class ArticlesController < ApplicationController
  end
 
  def new
-    @article = Article.new(article_params)
+    @article = Article.new
  end
 
  def create
    @article = Article.new(article_params)
-
    if @article.save
       redirect_to @article
    else
