793ac85ebb6d93b0627227c20325f629aabbb476
Colin Di Meo <dimeo.colin@gmail.com>
2017-09-18 19:00:29 -0400

    comments can be deleted - auth implemented for post creation, all stable

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+ http_basic_authenticate_with name: 'dhh', password: 'secret',
+                              except: %i[index show]
+
  def index
   @articles = Article.all
  end
@@ -43,6 +46,7 @@ class ArticlesController < ApplicationController
  end
 
  private
+
  def article_params
   params.require(:article) .permit(:title, :text)
  end
